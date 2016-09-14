4b3f9799d8106034ed36434babe9bf733f2d22ea
padpadpadpad <d.padfieldscfc@gmail.com>
2016-09-14 18:57:40 +0100

    adding total elevation gain to facet

--- R/get_elev_prof.R
+++ R/get_elev_prof.R
@@ -38,7 +38,7 @@ get_elev_prof <- function(act_data, acts = 1, key, total = FALSE){
 	# create a dataframe of long and latitudes
 	lat_lon <- get_all_LatLon(id_col = 'upload_id', parent_data = MyActs) %>%
 	  dplyr::full_join(., MyActs, by = 'upload_id') %>%
-	  dplyr::select(., upload_id, type, start_date, lat, lon, location_city)
+	  dplyr::select(., upload_id, type, start_date, lat, lon, location_city, total_elevation_gain)
 	
 	distances <- dplyr::group_by(lat_lon, upload_id) %>%
 	  dplyr::do(data.frame(distance = get_dists(.)))
@@ -49,7 +49,7 @@ get_elev_prof <- function(act_data, acts = 1, key, total = FALSE){
 
 	lat_lon$start_date <- gsub('T.*$', '', lat_lon$start_date) %>% 
 		as.Date(format = '%Y-%m-%d')
-	lat_lon <- tidyr::unite(lat_lon, 'facets', upload_id, start_date, sep = ', ')
+	lat_lon <- tidyr::unite(lat_lon, 'facets', upload_id, start_date, total_elevation_gain, sep = ', ')
 
 	ylab <- 'Elevation (m)'
 	
