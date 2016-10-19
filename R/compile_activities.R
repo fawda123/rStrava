5225be242bb2d096570db5b0a07d578dc0a779e6
fawda123 <mbafs2012@gmail.com>
2016-10-19 08:02:09 -0500

    better handling of column numeric conversion in compile_activities

--- R/compile_activities.R
+++ R/compile_activities.R
@@ -40,9 +40,11 @@ compile_activities <- function(actlist, acts = NULL, units = 'metric'){
 	temp <- unlist(actlist)
 	att <- unique(attributes(temp)$names)
 	out <- plyr::ldply(actlist, compile_activity, columns = att)
-	
+
 	# convert relevant columns to numeric
-	out <- dplyr::mutate_at(out, c('achievement_count', 'athlete.resource_state', 'athlete_count', 'average_speed', 'comment_count', 'distance', 'elapsed_time', 'elev_high', 'elev_low', 'end_latlng1', 'end_latlng2', 'kudos_count', 'map.resource_state', 'max_speed', 'moving_time', 'photo_count', 'resource_state', 'start_latitude', 'start_latlng1', 'start_latlng2', 'start_longitude', 'total_elevation_gain', 'total_photo_count'), as.numeric)
+	cols <- c('achievement_count', 'athlete.resource_state', 'athlete_count', 'average_speed', 'average_watts', 'comment_count', 'distance', 'elapsed_time', 'elev_high', 'elev_low', 'end_latlng1', 'end_latlng2', 'kilojoules', 'kudos_count', 'map.resource_state', 'max_speed', 'moving_time', 'photo_count', 'resource_state', 'start_latitude', 'start_latlng1', 'start_latlng2', 'start_longitude', 'total_elevation_gain', 'total_photo_count')
+	cols <- names(out)[names(out) %in% cols]
+	out <- dplyr::mutate_at(out, cols, as.numeric)
 	
 	if(units == 'metric'){
 		
