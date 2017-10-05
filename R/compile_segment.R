302c43d44b6a9618538bbdbda87c2c08efc56788
Daniel Padfield <d.padfieldscfc@gmail.com>
2017-10-05 22:26:36 +0100

    adding compile segment functionality

--- R/compile_segment.R
+++ R/compile_segment.R
@@ -1,5 +1,17 @@
-compile_segment <- function(x, columns, stoken){
-	temp <- rStrava::get_segment(stoken, id = x)
+#' Compile information on a segment
+#' 
+#' Compile generation information on a segment
+#' 
+#' @param segment_id a Strava segment id
+#' @param stoken A \code{\link[httr]{config}} object created using the \code{\link{strava_oauth}} function
+#' 
+#' @concept token
+#' @details compiles information a segment by calling \code{\link{get_segment}} internally
+#' @return dataframe of all information given in a call from \code{\link{get_segment}}
+#' @export
+compile_segment <- function(segment_id, stoken){
+
+	temp <- rStrava::get_segment(stoken, id = segment_id)
 	temp <- data.frame(unlist(temp), stringsAsFactors = F)
 	temp$ColNames <- rownames(temp)
 	temp <- tidyr::spread(temp, ColNames, unlist.temp.)
