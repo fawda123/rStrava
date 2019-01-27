e386685054e86e119a68cef46f260a5d18859e9b
fawda123 <mbafs2012@gmail.com>
2019-01-27 09:37:58 -0800

    added id option for individual user to get_activity_list

--- R/get_activity_list.R
+++ R/get_activity_list.R
@@ -7,7 +7,7 @@
 #' @param club logical if you want the activities of a club
 #' @param friends logical if you want friends' activities of the authenticated user
 #' 
-#' @details Requires authentication stoken using the \code{\link{strava_oauth}} function and a user-created API on the strava website.
+#' @details Requires authentication stoken using the \code{\link{strava_oauth}} function and a user-created API on the strava website.  If retrieving activities using individual \code{id} values, the output list returned contains additional information from the API and the results have not been tested with the functions in this package.  It is better practice to retrieve all activities (as in the example below), use \code{\link{compile_activities}}, and then filter by individual activities.
 #' 
 #' @return A list of activities for further processing or plotting.
 #' 
@@ -34,13 +34,28 @@ get_activity_list <- function(stoken, id = NULL, club = FALSE, friends = FALSE){
 	
 	#This codes assumes requesting all the pages of activities. In other circunstances change the parameters of 'get_pages'
 	
-	if (friends | club){
-		dataRaw <- get_pages(url_activities(id = id, club = club, friends=friends), stoken, per_page = 200, page_id = 1, page_max = 1)
-	}
-	else{
-		dataRaw <- get_pages(url_activities(), stoken, All=TRUE)
-	}
+	# get individual id activities for user
+	if(!is.null(id) & !any(friends | club)){
+		dataRaw <- list()
+		
+		for(i in id){
+			
+			tmp <- get_pages(url_activities(id = i), stoken, All=TRUE)
+			dataRaw <- c(dataRaw, list(tmp))
+			
+		}
+		
+	# otherwise get all
+	} else {
+		
+		if (friends | club){
+			dataRaw <- get_pages(url_activities(id = id, club = club, friends=friends), stoken, per_page = 200, page_id = 1, page_max = 1)
+		} else {
+			dataRaw <- get_pages(url_activities(id = id), stoken, All=TRUE)
+		}
 	
+	}
+		
 	return(dataRaw)
 	
 }
