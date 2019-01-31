80b59bfbdf28b6874da49da8e3ff0eb07d9f1e59
fawda123 <mbafs2012@gmail.com>
2019-01-31 09:14:25 -0800

    removed features related to getting follower data

--- R/get_activity_list.R
+++ R/get_activity_list.R
@@ -1,11 +1,10 @@
 #' Get an activities list
 #'
-#' Get an activities list of the desired type (club, friends, user)
+#' Get an activities list of the desired type (club, user)
 #'
 #' @param stoken A \code{\link[httr]{config}} object created using the \code{\link{strava_oauth}} function
 #' @param id string for id of the activity or club if \code{club = TRUE}
 #' @param club logical if you want the activities of a club
-#' @param friends logical if you want friends' activities of the authenticated user
 #' 
 #' @details Requires authentication stoken using the \code{\link{strava_oauth}} function and a user-created API on the strava website.  If retrieving activities using individual \code{id} values, the output list returned contains additional information from the API and the results have not been tested with the functions in this package.  It is better practice to retrieve all activities (as in the example below), use \code{\link{compile_activities}}, and then filter by individual activities.
 #' 
@@ -26,16 +25,13 @@
 #' 
 #' get_activity_list(stoken)
 #' }
-get_activity_list <- function(stoken, id = NULL, club = FALSE, friends = FALSE){
+get_activity_list <- function(stoken, id = NULL, club = FALSE){
 	#stoken:  Configured token (output from config(token = strava_oauth(...)))
 	#id:      ID of the activity or club if club=TRUE (string)
-	#friends: TRUE if you want the friends activities of the authenticated user (logic)
 	#club:    TRUE if you want the activities of a club (logic)
 	
-	#This codes assumes requesting all the pages of activities. In other circunstances change the parameters of 'get_pages'
-	
 	# get individual id activities for user
-	if(!is.null(id) & !any(friends | club)){
+	if(!is.null(id) & !club){
 		dataRaw <- list()
 		
 		for(i in id){
@@ -48,8 +44,8 @@ get_activity_list <- function(stoken, id = NULL, club = FALSE, friends = FALSE){
 	# otherwise get all
 	} else {
 		
-		if (friends | club){
-			dataRaw <- get_pages(url_activities(id = id, club = club, friends=friends), stoken, per_page = 200, page_id = 1, page_max = 1)
+		if (club){
+			dataRaw <- get_pages(url_activities(id = id, club = club), stoken, per_page = 200, page_id = 1, page_max = 1)
 		} else {
 			dataRaw <- get_pages(url_activities(id = id), stoken, All=TRUE)
 		}
--- R/url_activities.R
+++ R/url_activities.R
@@ -3,7 +3,6 @@
 #' Set the url of activities for different activity lists
 #' 
 #' @param id string for id of the activity or club if \code{club = TRUE}
-#' @param friends logical if you want friends' activities of the authenticated user
 #' @param club logical if you want the activities of a club
 #'
 #' @details This function concatenates appropriate strings so no authentication token is required.  This is used internally by other functions.
@@ -25,7 +24,7 @@
 #' 
 #' url_activities(2837007)
 #' }
-url_activities <- function(id = NULL, friends = FALSE, club = FALSE){
+url_activities <- function(id = NULL, club = FALSE){
 	
 	url_ <- "https://www.strava.com/api/v3/activities/"
 	if(!is.null(id)){
@@ -35,9 +34,6 @@ url_activities <- function(id = NULL, friends = FALSE, club = FALSE){
 		else{#Url for an specific activity
 			url_ <- paste(url_, id, sep = "")
 		}
-	}
-	else if(friends){#Url for the activities of the authenticated user's friends
-		url_ <- paste(url_,"following", sep = "")
 	}
 	else{#Url for the list of activities of the authenticated user
 		url_ <- paste(url_athlete(),"/activities", sep = "")
