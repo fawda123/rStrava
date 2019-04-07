7c8877f9f2396a1f8af81fbebf4f94f0415dc83d
fawda123 <mbafs2012@gmail.com>
2019-04-07 09:46:42 -0700

    exception fix for achievement_fun and recent_fun if not found, fix to yesterday date in recent_fun

--- R/achievement_fun.R
+++ R/achievement_fun.R
@@ -8,6 +8,9 @@
 #' 
 achievement_fun <- function(prsd){
 	
+	if(length(prsd$achievements) == 0)
+		return(list())
+	
 	out <- prsd$achievements[, c('description', 'timeago')]
 	out$timeago <- gsub('^.*\"timeago\"\\sdatetime=\\"(.*Z)\\".*$', '\\1', out$timeago)
 	out$timeago <- as.POSIXct(gsub('T|Z', ' ', out$timeago), 'GMT')
--- R/recent_fun.R
+++ R/recent_fun.R
@@ -9,9 +9,13 @@
 #' @concept notoken
 recent_fun <- function(prsd){
 	
+	if(is.null(prsd$recentActivities))
+		return(list)
+	
 	out <- prsd$recentActivities[, c('id', 'name', 'type', 'startDateLocal', 'distance', 'elevation', 'movingTime')]
 	out$distance <- as.numeric(gsub('^(.*)\\s.*$', '\\1', out$distance))
 	out$elevation <- as.numeric(gsub('^(.*)\\s.*$', '\\1', out$elevation))
+	out$startDateLocal[out$startDateLocal %in% 'Yesterday'] <- format(Sys.Date() - 1, '%B %d, %Y')
 	out$startDateLocal <- as.Date(out$startDateLocal, '%B %d, %Y')
 
 	return(out)
