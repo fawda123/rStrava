6da1858a9719b79fb398ef05492edb42cb0e7b92
fawda123 <mbafs2012@gmail.com>
2019-06-02 09:20:03 -0700

    enhancements for get_segment and compile_segment for correct request arguments

--- R/compile_segment.R
+++ R/compile_segment.R
@@ -2,18 +2,44 @@
 #' 
 #' Compile generation information on a segment
 #' 
-#' @param segment_id a Strava segment id
-#' @param stoken A \code{\link[httr]{config}} object created using the \code{\link{strava_oauth}} function
+#' @param seglist a Strava segment list returned by \code{\link{get_segment}}
 #' 
 #' @concept token
-#' @details compiles information a segment by calling \code{\link{get_segment}} internally
+#' @details compiles information for a segment
 #' @return dataframe of all information given in a call from \code{\link{get_segment}}
 #' @export
-compile_segment <- function(segment_id, stoken){
+#' 
+#' @examples 
+#' \dontrun{
+#' # create authentication token
+#' # requires user created app name, id, and secret from Strava website
+#' stoken <- httr::config(token = strava_oauth(app_name, app_client_id, 
+#' 	app_secret, cache = TRUE))
+#' 
+#' get_segment(stoken, id = 229781) %>% compile_segment
+#' 
+#' get_segment(stoken, id = 229781, request = "leaderboard") %>% compile_segment
+#' }
+compile_segment <- function(seglist){
 
-	temp <- rStrava::get_segment(stoken, id = segment_id)
-	temp <- data.frame(unlist(temp), stringsAsFactors = F)
-	temp$ColNames <- rownames(temp)
-	temp <- tidyr::spread(temp, ColNames, unlist.temp.)
-	return(temp)
+	if('entries' %in% names(seglist)){
+
+		out <- tibble::enframe(seglist$entries) %>%
+			dplyr::mutate(value = purrr::map(value, function(x) as.data.frame(x, stringsAsFactors = FALSE))) %>%
+			tidyr::unnest() %>%
+			dplyr::select(-name) %>%
+			as.data.frame(stringsAsFactors = FALSE)
+
+	}
+
+	if(is.null(names(seglist))){
+		
+		out <- tibble::enframe(seglist) %>% 
+			dplyr::mutate(value = purrr::map(value, function(x) x %>% unlist %>% tibble::enframe(.) %>% tidyr::unnest(.) %>% tidyr::spread('name', 'value')))
+		out <- dplyr::bind_rows(out$value)
+		
+	}
+	
+	return(out)
+	
 }
--- R/get_segment.R
+++ R/get_segment.R
@@ -4,9 +4,9 @@
 #'
 #' @param stoken A \code{\link[httr]{config}} object created using the \code{\link{strava_oauth}} function
 #' @param id numeric for id of the segment
-#' @param request chr string, must be "starred", "all_efforts", "leaderboard", "explore" or NULL for segment details
+#' @param request chr string, must be "starred", "leaderboard", "all_efforts", or NULL for segment details
 #' 
-#' @details Requires authentication stoken using the \code{\link{strava_oauth}} function and a user-created API on the strava website.
+#' @details Requires authentication stoken using the \code{\link{strava_oauth}} function and a user-created API on the strava website.  The authenticated user must have an entry for a segment to return all efforts if \code{request = "all_efforts"}. For \code{request = "starred"}, set \code{id = NULL}.
 #' 
 #' @return Data from an API request.
 #' 
@@ -16,6 +16,7 @@
 #' 
 #' @import httr
 #' 
+#' @seealso \code{\link{compile_segment}} for converting the \code{list} output to \code{data.frame}
 #' @examples
 #' \dontrun{
 #' # create authentication token
@@ -23,10 +24,19 @@
 #' stoken <- httr::config(token = strava_oauth(app_name, app_client_id, 
 #' 	app_secret, cache = TRUE))
 #' 
+#' # get segment details
 #' get_segment(stoken, id = 229781)
+#' 
+#' # get leaderboard
 #' get_segment(stoken, id = 229781, request = 'leaderboard')
+#' 
+#' # get starred segments for the authenticated user
+#' get_segment(stoken, request = 'starred')
 #' }
 get_segment <- function(stoken, id = NULL, request = NULL){
+
+	if(!is.null(id) & request == 'starred')
+		stop('id must be NULL if request = "starred"')
 	
 	dataRaw <- get_basic(url_segment(id, request = request), stoken)
 	return(dataRaw)
